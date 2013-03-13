5b7bc00367bd7453632fdc7dd0a213c7c4489019
atc <atc@alexcollins.org>
2013-03-13 09:20:00 +0000

    Initial commit

---
+++ src/test/java/jdk8/lambda/demo/collections/ReduceTest.java
@@ -0,0 +1,17 @@
+package jdk8.lambda.demo.collections;
+
+import org.junit.Test;
+
+/**
+ * Created with IntelliJ IDEA.
+ * User: atc
+ * Date: 13/03/13
+ * Time: 08:33
+ * To change this template use File | Settings | File Templates.
+ */
+public class ReduceTest {
+    @Test
+    public void testSum() throws Exception {
+
+    }
+}
